3f85467392e69872ec12e70bec7d7bbb2463f45a
Andre Peixoto <peixoto.amp@gmail.com>
2021-04-04 01:20:36 -0300

    Styling changes: TeacherItem and TeachersList

--- src/components/TeacherItem/index.tsx
+++ src/components/TeacherItem/index.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
 
-import './styles.css'
+import './styles.css';
 
 function TeacherItem() {
   return (
     <article className="teacher-item">
       <header>
-        <img src="https://avatars1.githubusercontent.com/u/35813862?s=460&v=4" alt="Andre Peixoto"/>
+        <img
+          src="https://avatars1.githubusercontent.com/u/35813862?s=460&v=4"
+          alt="Andre Peixoto"
+        />
         <div>
           <strong>Andre Peixoto</strong>
           <span>English</span>
@@ -17,17 +20,18 @@ function TeacherItem() {
 
       <p>
         Enthusiast of human and computer languages.
-        <br/> <br/>
-        Loves to teach what he learns and to help others in their endeavors to becoming more skilled; more satisfied.
+        <br /> <br />
+        Loves to teach what he learns and to help others in their endeavors to
+        becoming more skilled; more satisfied.
       </p>
 
       <footer>
         <p>
-          Preço/hora
+          Price
           <strong>$ 60/hour</strong>
         </p>
         <button type="button">
-          <img src={whatsappIcon} alt="WhatsApp"/>
+          <img src={whatsappIcon} alt="WhatsApp" />
           Contact
         </button>
       </footer>
